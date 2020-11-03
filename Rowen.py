e01cc0f8ebd3e987446db9bb4766480186dfb26a
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2020-11-03 11:23:37 -0500

    Update Rowen.py

--- Rowen.py
+++ Rowen.py
@@ -1,1 +1,19 @@
-print("Rowen")
+print("Rowen Thompson")
+print("CPT051")
+print("Rowen.py")
+print("October 2020")
+print("------------------------")
+
+Minecraft_Price = 200 
+Minecraft_dungeons_Price = 400
+Call_of_duty_warfare_Price = 100
+
+
+
+
+print("What are you going to buy?")
+print("Minecraft :" +)
+print("Minecraft dungeons :" +)
+print("Call of duty warfare :" +)
+print("Escape of tarkov :" + str( )
+print("Total War : ROME 2 :" +)
