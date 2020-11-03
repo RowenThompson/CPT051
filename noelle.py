aab340660c8cc8c30603bd2ac2d6a965ab5acc84
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2020-11-03 11:11:56 -0500

    Update noelle.py

--- noelle.py
+++ noelle.py
@@ -1,1 +1,20 @@
-print("noelle")
+print("Noelle Adkin")
+print("CPT051")
+print("noelle.py")
+print("October 2020")
+print("--------------------")
+
+TAX = .01
+CURRENCY = " Gold pieces"
+
+wizard_hat_price = 100
+magic_sword_price = 200
+health_potion = 10
+
+
+print("What are you buying?")
+print("Wizard hat : " + str(wizard_hat_price) + CURRENCY)
+print("Magic Sword : " + str(magic_sword_price) + CURRENCY)
+print("Health Potion : " + str(health_potion) + CURRENCY)
+print("* Taxes not included")
+
