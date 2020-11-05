b251a76357091ac947f507d434063b8f73dabbd2
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2020-11-05 08:47:22 -0500

    Update Rowen.py

--- Rowen.py
+++ Rowen.py
@@ -5,6 +5,7 @@ print("October 2020")
 print("------------------------")
 
 CURRENCY = " Iron"
+TAX = 0.01
 
 Minecraft_Price = 200 
 Minecraft_dungeons_Price = 400
@@ -14,6 +15,18 @@ Call_of_duty_warfare_Price = 100
 
 
 print("What are you going to buy?")
-print("Minecraft :" + str(Minecraft_Price) + CURRENCY)
-print("Minecraft dungeons :" + str(Minecraft_dungeons_Price) + CURRENCY)
-print("Call of duty warfare :" + str(Call_of_duty_warfare_Price) + CURRENCY)
+print("Minecraft : " + str(Minecraft_Price) + CURRENCY)
+print("Minecraft dungeons : " + str(Minecraft_dungeons_Price) + CURRENCY)
+print("Call of duty warfare : " + str(Call_of_duty_warfare_Price) + CURRENCY)
+print("Taxes not included")
+
+def take_input():
+    print("Enter your selection here")
+    selection = input()
+    return selection
+
+menu_select = take_input()
+
+print("You selected : " + menu_select)
+print("First letter : " + menu_select)
+hello
