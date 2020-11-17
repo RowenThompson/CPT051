aa4514ae78caa944f126a897e035d872d098d17a
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2020-11-17 12:20:00 -0500

    Create New Store!!!.py

---
+++ New Store!!!.py
@@ -0,0 +1,11 @@
+print("Hello welcome to New Store!!!")
+print("You ready to see what you gonna get?")
+print("You got : ")
+
+import random
+Random = ['gravel', 'obsidian', 'microphone', 'neckless', 'nothing']
+print(random.choice(Random))
+
+
+
+
