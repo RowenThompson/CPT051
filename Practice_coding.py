8102caa50fd58a15a7d06fd9e9d94cc7fd981b83
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2020-12-08 14:23:40 -0500

    Create Practice_coding.py
    Just practicing

---
+++ Practice_coding.py
@@ -0,0 +1,37 @@
+Is_gender = False
+Hungry_morning = False
+Go_to_school = False
+See_friend_at_school = False
+numm = 0
+
+if Is_gender:
+    print("Yeah! BOII!!!!")
+else:
+    print("I'm a girl too!")
+
+print("Let's go to school!")
+
+if Hungry_morning and Go_to_school:
+    print("Gotta eat first sorry!")
+    print("Ok. See you there?")
+else:
+    print("ok!")
+
+while numm <= 10:
+    print("Not done!!!")
+    print(numm)
+    numm = numm + 1
+
+full_name = input('what is your full name? ')
+full_name = input('what is your full name? ')
+print('Hi ' + full_name)
+nickname = input('what do you like to be called? ')
+print('Ok thanks ' + nickname)
+age = input('how old are you? ')
+print('Thanks ' + nickname)
+animals = input('what type and how many animals do you have for us to help? ')
+print('thanks we will do our best to treat your ' + animals)
+hurt = input('what is wrong with your pet(s)? ')
+print('ok thanks we will do our best')
+aniplay = input('Do you want us to play with your? ' + animals)
+print('ok')
