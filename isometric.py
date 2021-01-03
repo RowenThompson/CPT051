eab2d1fd3c73bb2a70eba0bc70bc5e022b99bb45
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2021-01-03 17:14:38 -0500

    game_

---
+++ isometric.py
@@ -0,0 +1,59 @@
+import pygame, sys, time, random
+
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((900, 900),0,32)
+display = pygame.Surface((300, 300))
+
+grass_img = pygame.image.load('grass.png').convert()
+grass_img.set_colorkey((0, 0, 0))
+
+
+player_x = 5
+player_y = 5
+vel = 1
+
+
+
+f = open('map.txt')
+map_data = [[int(c) for c in row] for row in f.read().split('\n')]
+f.close()
+
+while True:
+    display.fill((0,0,0))
+
+    for y, row in enumerate(map_data):
+        for x, tile in enumerate(row):
+            if tile:
+                #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
+                display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
+                if (x == player_x) and (y == player_y):
+                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        keys = pygame.key.get_pressed()
+    
+        if keys[pygame.K_LEFT]:
+            player_x -= vel
+
+        if keys[pygame.K_RIGHT]:
+            player_x += vel
+
+        if keys[pygame.K_UP]:
+            player_y -= vel
+
+        if keys[pygame.K_DOWN]:
+            player_y += vel
+
+    screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    pygame.display.update()
+    #time.sleep(1)
--- simple_movement.py
+++ simple_movement.py
@@ -2,6 +2,7 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((500,500))
+display = pygame.Surface((300, 300))
 pygame.display.set_caption("First Game")
 
 x = 50
@@ -9,6 +10,11 @@ y = 50
 width = 40
 height = 60
 vel = 5
+
+
+grass_img = pygame.image.load('grass.png').convert()
+grass_img.set_colorkey((0, 0, 0))
+
 
 run = True
 
@@ -34,7 +40,8 @@ while run:
         y += vel
     
     win.fill((0,0,0))  # Fills the screen with black
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))
+    #win.blit(grass_img, x, y)
     pygame.display.update() 
     
 pygame.quit()
