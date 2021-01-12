9a13072d6d88c3bd7f62c32f3bb4c39c3211b16d
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2021-01-12 16:51:40 -0500

    stuff

--- game_oop.py
+++ game_oop.py
@@ -18,10 +18,29 @@ class Player(Fighter):
     def __init__(self, name,  hp, x, y, attack, quest_points):
         super().__init__(name,  hp, x, y, attack)
         self.quest_points = quest_points
-        
+
+    def get_name(self):
+        print(self.name)
+
+class Caster:
+    def __init__(self, mana):
+        self.mana = mana
+
+    def spell(self):
+        print("You cast a spell")
 
 
+class Player_Caster(Player,Caster):
+    def __init__(self, name,  hp, x, y, attack, quest_points, mana):
+        super().__init__(name,  hp, x, y, attack, quest_points)
+        self.mana = mana
+    
+class Tile():
+    pass
 
+class Magic_Tile(Tile, Caster):
+    def __init__(self, mana):
+        super().__init__(mana)
 
 
 
@@ -43,3 +62,24 @@ p = Player("Rowen", 300, 2, 2, 60, 0)
 
 print("my_func")
 my_function(p)
+
+n = Player_Caster("Rowen", 300, 2, 2, 60, 0, 100)
+
+n.spell()
+
+n.get_name()
+
+print("tiles")
+u = Magic_Tile(50)
+u.spell()
+
+i.heal()
+i.drop()"You dropped *name of item*
+i.use()"You used
+i.get_name()
+
+#item: name, get_name
+#Caster: use/cast
+#Healing: amount_healed
+#Equipment: Equip
+
--- isometric.py
+++ isometric.py
@@ -3,16 +3,39 @@ import pygame, sys, time, random
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption('game base')
-screen = pygame.display.set_mode((900, 900),0,32)
+screen = pygame.display.set_mode((1000, 1000),0,32)
 display = pygame.Surface((300, 300))
+
+base_screen_size = [1000,1000]
+
+
 
 grass_img = pygame.image.load('grass.png').convert()
 grass_img.set_colorkey((0, 0, 0))
 
+mx = 5
+my = 5
+
+game_scroll = [0, 0]
 
 player_x = 5
 player_y = 5
 vel = 1
+
+
+def complex_camera(camera, target_rect):
+    # we want to center target_rect
+    x = -player_x + WIN_WIDTH/2 
+    y = -player_y + WIN_HEIGHT/2
+    # move the camera. Let's use some vectors so we can easily substract/multiply
+    camera.topleft += (pygame.Vector2((x, y)) - pygame.Vector2(camera.topleft)) * 0.06 # add some smoothness coolnes
+    # set max/min x/y so we don't see stuff outside the world
+    camera.x = max(-(camera.width-WIN_WIDTH), min(0, camera.x))
+    camera.y = max(-(camera.height-WIN_HEIGHT), min(0, camera.y))
+    
+    return camera
+
+
 
 
 
@@ -30,6 +53,7 @@ while True:
                 display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
                 if (x == player_x) and (y == player_y):
                     display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+                
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -40,17 +64,32 @@ while True:
                 pygame.quit()
                 sys.exit()
 
+        mx, my = pygame.mouse.get_pos()
+        true_mx = mx
+        true_my = my
+        mx -= (screen.get_width() - base_screen_size[0]) // 2
+        my -= (screen.get_height() - base_screen_size[1]) // 2
+        mx /= base_screen_size[0] / display.get_width()
+        my /= base_screen_size[1] / display.get_height()
+
+
+        game_mx = ((mx + game_scroll[0]) + (my + game_scroll[1])) / 18 * 100
+        game_my = ((-mx - game_scroll[0]) + (my + game_scroll[1])) / 18 * 100
+
+        if (x == game_mx) and (y == game_my):
+            display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+
         keys = pygame.key.get_pressed()
     
         if keys[pygame.K_LEFT]:
             player_x -= vel
-
+            
         if keys[pygame.K_RIGHT]:
             player_x += vel
-
+            
         if keys[pygame.K_UP]:
             player_y -= vel
-
+            
         if keys[pygame.K_DOWN]:
             player_y += vel
             
@@ -58,6 +97,8 @@ while True:
             player_y = 5
             player_x = 5
 
+    #camera = Camera(complex_camera, total_level_width, total_level_height)
     screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    #screen.blit(pygame.transform.scale(display, screen.get_size()), (player_x, player_y))
     pygame.display.update()
     #time.sleep(1)
