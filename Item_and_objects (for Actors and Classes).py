2d6c7f5a82a919e901a1138cfbd555be5ed057f6
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-01-14 15:14:55 -0500

    Create Item_and_objects (for Actors and Classes).py

---
+++ Item_and_objects (for Actors and Classes).py
@@ -0,0 +1,24 @@
+class Actor:
+    def __init__(self, name, hp, x , y):
+        self.name = name
+        self.hp = hp
+        
+class Item():
+    def __init__(name, get_name, number_of_items, use,):
+
+class Object():
+    def __init__(self, name, get_name, Object_brake):
+        self.name
+        pass
+
+class Caster(Actor):
+    def __init__(self, mana):
+        super().__init__(mana)
+        self.mana = mana
+
+def spell(self):
+        print("You cast a spell")
+
+t = Object()
+
+self = ("john, mama, lol")
