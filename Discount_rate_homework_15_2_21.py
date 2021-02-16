c7c158ac11cd0e0d5be2910708bd0752641fc78b
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-02-15 19:03:49 -0500

    Create Discount_rate_homework_15_2_21.py

---
+++ Discount_rate_homework_15_2_21.py
@@ -0,0 +1,18 @@
+DISCOUNT_NAME_MEMBER = "Member"
+DISCOUNT_NAME_SENIOR = "Senior"
+DISCOUNT_NAME_NONE = "No Discount"
+DISCOUNT_RATE_MEMBER = 0.15
+DISCOUNT_RATE_SENIOR = 0.25
+DISCOUNT_RATE_NONE = 0.0
+TAX_RATE = .075
+print("Welcome")
+userName = float(input("userName= "))
+itemName = float(input("itemName= "))
+originalPrice = float(input("originalPrice= "))
+howMany = float(input("howMany= "))
+menuSelection = float(input("menuSelection= "))
+
+if(menuSelection == 'A'):
+    print("You selected A")
+    if(meniSelection == Y):
+        print(DISCOUNT_RATE_MEMBER)
