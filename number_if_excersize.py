86899740c799e629ad0a743913e8644fc6a6fe43
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-02-16 15:24:44 -0500

    Create number_if_excersize.py

---
+++ number_if_excersize.py
@@ -0,0 +1,11 @@
+number = int(input("Type a number! "))
+
+if(number > 0):
+    print("The number is positive")
+
+elif(number < 0):
+    print("The number is negative")
+
+else:
+    print("The number is zero")
+
