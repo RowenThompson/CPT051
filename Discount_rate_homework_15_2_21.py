95f72f5184874168aadd9c94e0c45e8428127a75
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-02-15 22:07:09 -0500

    Update Discount_rate_homework_15_2_21.py

--- Discount_rate_homework_15_2_21.py
+++ Discount_rate_homework_15_2_21.py
@@ -15,31 +15,33 @@ menuSelection = str(input("menuSelection= "))
 if(menuSelection == 'A'):
     print("Welcome back member!")
     discountRate = DISCOUNT_RATE_MEMBER
+
 elif(menuSelection == 'B'):
     print("You selected B")
-    if(menuSelection == 'N'):
-        discountRate = DISCOUNT_RATE_NONE
-    #elif(menuSelection == 'Y'):
-        #discountRate = DISCOUNT_RATE_SENIOR
+    discountRate = DISCOUNT_RATE_SENIOR
 
-#discountAmount = originalPrice * discountRate
-#discountPrice = originalPrice - discountAmount
-#subTotal = howMany * discountPrice
-#tax = subTotal * TAX_RATE
-#totalCost = subTotal + tax
+else:
+    print("LOL YOU SUCK")
+    discountRate = DISCOUNT_RATE_NONE
 
-#print(str((discountRate * 100)) + "%" )
+discountAmount = originalPrice * discountRate
+discountPrice = originalPrice - discountAmount
+subTotal = howMany * discountPrice
+tax = subTotal * TAX_RATE
+totalCost = subTotal + tax
 
-#if(totalCost > 0.0):
-    #print(str(userName))
-    #print(str(itemName))
-    #print(str(originalPrice))
-    #print(str(discountRate))
-    #print(str(discountAmount))
-    #print(str(discountPrice))
-    #print(str(howMany))
-    #print(str(subTotal))
-    #print(str(tax))
-    #print(str(totalCost))
+print(str((discountRate * 100)) + "%" )
+
+if(totalCost > 0.0):
+    print(str("User              " + userName))
+    print(str("Item Name         " + itemName))
+    print(str("Original Price   $" + originalPrice))
+    print(str(("Discount         " + discountRate * 100)) + "%" )
+    print(str(discountAmount))
+    print(str(discountPrice))
+    print(str(howMany))
+    print(str(subTotal))
+    print(str(tax))
+    print(str(totalCost))
 print("Good bye!")
-#print(discountRate)
+print(discountRate)
