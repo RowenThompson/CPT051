1d0182a8d01eccb6563cde13461787ad96d6e5bf
N Adkin <50452002+NAdkin@users.noreply.github.com>
2021-02-25 15:35:17 -0500

    Update Odd_or_even_while_loop.py

--- Odd_or_even_while_loop.py
+++ Odd_or_even_while_loop.py
@@ -1,7 +1,10 @@
+number = 0
+
 while(True):
+    number = int(input("Type a number! "))
+    
     if(number % 2 == 0):
         print("It is an even number!")
     else:
         print("It is an odd number!")
 
-number = int(input("Type a number! "))
