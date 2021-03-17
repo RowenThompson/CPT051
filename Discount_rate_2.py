a7b0b3a6ccffb17f18862cc47b4018b7f56c1023
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-03-17 15:27:20 -0400

    Update Discount_rate_2.py

--- Discount_rate_2.py
+++ Discount_rate_2.py
@@ -5,41 +5,37 @@ DISCOUNT_RATE_MEMBER = 0.15
 DISCOUNT_RATE_SENIOR = 0.25
 DISCOUNT_RATE_NONE = 0.0
 TAX_RATE = .075
-discountRate = 0
-menuSelection = n
-print("Welcome")
 
-def take_input():
-    print("Enter your selection here")
-    menuSelection = input()
-    menuSelection = menuSelection.upper()
-    menuSelection = menuSelection[0]
-    return selection
+menuSelection = 'U'
+discountRate = (float('.0'))
+print("Welcome")
 
 userName = str(input("userName = "))
 itemName = str(input("itemName = "))
 originalPrice = float(input("originalPrice = "))
 howMany = float(input("howMany = "))
-#menuSelection = str(input("menuSelection = "))
-
-def take_input():
-    menuSelection = menuSelection.upper()
-    menuSelection = menuSelection[0]
-    return selection
-
+menuSelection = input("menuSelection = ")
+#def take_input():
+    #print("Enter your selection here")
+    #selection = input()
+    #selection = selection.upper()
+    #selection = selection[0]
+    #return selection
 
 if(menuSelection == 'A'):
     print("Hello Member")
     discountRate = DISCOUNT_RATE_MEMBER
-
+    #menuSelection.append('B')
+    
 elif(menuSelection == 'B'):
     print("Hello Senior")
     discountRate = DISCOUNT_RATE_SENIOR
-
+    #menuSelection.append('B')
+    
 else:
     print("No Discount")
     DiscountRate = DISCOUNT_RATE_NONE
-
+    #menuSelection.append('Something')
 discountAmount = originalPrice * discountRate
 discountPrice = originalPrice - discountAmount
 subTotal = howMany * discountPrice
@@ -47,6 +43,17 @@ tax = subTotal * TAX_RATE
 totalCost = subTotal + tax
 
 if(totalCost > 0.0):
-    print(userName)
-    
-take_input()
+    print("User: " + str(userName))
+    print("itemName: " + str(itemName))
+    print("originalPrice: " + str(originalPrice))
+    print("discountRate: " + str(discountRate))
+    print("discountAmount: " + str(discountAmount))
+    print("discountPrice:" + str(discountPrice))
+    print("howMany: " + str(howMany))
+    print("subTotal: " + str(subTotal))
+    print("tax: " + str(tax))
+    print("totalCost: " + str(totalCost))
+
+#take_input()
+
+print(menuSelection)
