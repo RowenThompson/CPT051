4414187ba69efb9b743b3aea4a5e46b58e07ee65
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2021-04-06 16:16:16 -0400

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,53 @@
+LENGTH_RESTRICTION = .50
+WIDTH_RESTRICTION = .75
+SOD_NAME_A = "South Carolina Rye"
+SOD_NAME_B = "Kentucky Bluegrass"
+SOD_PRICE_A = .57
+SOD_PRICE_B = 1.47
+HEATER_NAME_A = "Unit A3100"
+HEATER_NAME_B = "Unit B8000"
+HEATER_NAME_C = "Unit C15000"
+HEATER_NAME_D = "Unit D25000"
+HEATER_NAME_E = "Unit E25000+"
+HEATER_CAP_A = 3100.0
+HEATER_CAP_B = 8000.0
+HEATER_CAP_C = 15000.0
+HEATER_CAP_D = 25000.0
+POOL_DEPTH = 6
+
+def welcomeBanner():
+    print("Welcome!")
+
+def take_input():
+    print("Enter your selection here")
+    selection = input()
+    selection = selection.upper()
+    selection = selection[0]
+    return selection
+
+#start
+
+welcomeBanner()
+
+userName = input("userName = ")
+propertyLength = float(input("propertyLength = "))
+propertyWidth = float(input("propertyWidth = "))
+
+propertySquareFootage = propertyLength * propertyWidth
+houseLength = propertyLength * LENGTH_RESTRICTION
+
+print(type(userName))
+print(type(propertyLength))
+print(type(propertyWidth))
+print(type(propertySquareFootage))
+print(type(LENGTH_RESTRICTION))
+print(type(houseLength))
+
+
+print(userName)
+print(str(propertyLength))
+print(str(propertyWidth))
+print(str(propertySquareFootage))
+print(str(propertyLength))
+print(LENGTH_RESTRICTION)
+print(houseLength)
